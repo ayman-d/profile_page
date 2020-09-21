1c093532a20fd9d6e922e251b3ad2ce2949ad6a6
aymand <aymand1982@gmail.com>
2020-09-20 21:28:13 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const project1 = document.querySelector("#project1");
+const project2 = document.querySelector("#project2");
+const project3 = document.querySelector("#project3");
+
+console.log(project1);
+console.log(project2);
+console.log(project3);
+
+project1.addEventListener("click", function () {
+  location.href = "http://www.suerecipes.com";
+});
+
+project2.addEventListener("click", function () {
+  location.href = "http://www.manar-blog.com";
+});
+
+project3.addEventListener("click", function () {
+  location.href = "http://www.incentiveuk.com";
+});
