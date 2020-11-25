b1e03cd6b17d0aa58efe72fb793828f882b83354
aymand <aymand1982@gmail.com>
2020-11-25 01:20:36 -0700

    added ticket tracker and made some styling changes

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const project1 = document.querySelector("#project1");
-const project2 = document.querySelector("#project2");
-const project3 = document.querySelector("#project3");
+// const project1 = document.querySelector("#project1");
+// const project2 = document.querySelector("#project2");
+// const project3 = document.querySelector("#project3");
 
-project1.addEventListener("click", function () {
-  location.href = "http://www.suerecipes.com";
-});
+// project1.addEventListener("click", function () {
+//   location.href = "http://www.suerecipes.com";
+// });
 
-project2.addEventListener("click", function () {
-  location.href = "http://www.manar-blog.com";
-});
+// project2.addEventListener("click", function () {
+//   location.href = "http://www.manar-blog.com";
+// });
 
-project3.addEventListener("click", function () {
-  location.href = "http://www.incentiveuk.com";
-});
+// project3.addEventListener("click", function () {
+//   location.href = "http://www.incentiveuk.com";
+// });
