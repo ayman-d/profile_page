1dfd52343906dc3ee9337abd0a6e84fffff96a19
Ayman Al-Dali <aymand1982@gmail.com>
2021-11-10 16:57:08 -0700

    remove api test

--- main.js
+++ main.js
@@ -13,25 +13,3 @@
 // project3.addEventListener("click", function () {
 //   location.href = "http://www.incentiveuk.com";
 // });
-
-var myHeaders = new Headers();
-myHeaders.append("clientId", "44f90130-92c5-4181-88d6-27692ee4ed7c");
-myHeaders.append("Access-Control-Request-Headers", "*");
-myHeaders.append(
-  "Authorization",
-  "Basic Y3d0ZXN0K2ZMbkxpcXU1SXVtTktqZVE6dGxWbFFsRlhMUEdaQkIyNg=="
-);
-
-var requestOptions = {
-  method: "GET",
-  headers: myHeaders,
-  redirect: "follow",
-};
-
-fetch(
-  "https://cwtest.eswit.com/v4_6_release/apis/3.0/service/tickets",
-  requestOptions
-)
-  .then((response) => response.text())
-  .then((result) => console.log(result))
-  .catch((error) => console.log("error", error));
